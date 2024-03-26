21ed37d38250784f58cb1cc8711f88a2de52f9c8
LinaMVG <limarcegarzon@gmail.com>
2024-03-26 15:47:46 -0500

    pruebas ailadas y pruebas con componente padre

--- src/app/components/people/people.component.spec.ts
+++ src/app/components/people/people.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeopleComponent } from './people.component';
+import { PersonComponent } from '../person/person.component';
+import { Person } from '../../models/person.model';
+import { By } from '@angular/platform-browser';
 
 describe('PeopleComponent', () => {
   let component: PeopleComponent;
@@ -8,10 +11,10 @@ describe('PeopleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PeopleComponent]
+      imports: [PeopleComponent, PersonComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(PeopleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +23,30 @@ describe('PeopleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a list app-person components',()=>{
+    component.people=[
+      new Person('name1','firstname1',22,1,1),
+      new Person('name2','firstname2',23,4,1.5),
+      new Person('name3','firstname3',24,12,1.3)
+    ]
+
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement.queryAll(By.css('app-person'));
+
+    expect(debugElement.length).toEqual(3);
+  });
+
+  it('should raise selected event when clicked',()=>{
+    const butDebug = fixture.debugElement.query(By.css('app-person .btn-choose'));
+
+
+    butDebug.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    const liDe = fixture.debugElement.query(By.css('.selectedPerson ul> li'));
+
+    expect(component.selectedPerson).toEqual(component.people[0]);
+    expect(liDe.nativeElement.textContent).toContain(component.selectedPerson?.name)
+  });
+
 });
--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Person } from '../../models/person.model';
-import { PersonComponent } from "../person/person.component";
+import { PersonComponent } from '../person/person.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-people',
     standalone: true,
+    imports: [PersonComponent,CommonModule],
     templateUrl: './people.component.html',
     styleUrl: './people.component.scss',
-    imports: [PersonComponent]
 })
-export class PeopleComponent {
-  person: Person = new Person('Lina', 'Velasquez',24,55,1.60)
 
+export class PeopleComponent implements OnInit {
+  people: Person[] = [
+    new Person('Lina', 'Velasquez', 24, 55, 1.6),
+    new Person('Marcela', 'Velasquez', 12, 42, 1.4),
+  ];
+  selectedPerson: Person | null = null;
+
+  constructor(){};
+
+  choose(person: Person) {
+    this.selectedPerson = person;
+  }
+
+  ngOnInit():void{};
 }
